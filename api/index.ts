0e56c56b7b3b5d998c933767647290a44f761d72
Digitalbradley <digitalbradley@gmail.com>
2025-07-10 20:31:12 -0400

    Create api index.ts

---
+++ api/index.ts
@@ -0,0 +1,21 @@
+import express, { type Request, Response, NextFunction } from "express";
+import { registerRoutes } from "../server/routes";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Initialize routes
+(async () => {
+  await registerRoutes(app);
+})();
+
+// Error handler
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  const status = err.status || err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(status).json({ message });
+});
+
+// Export for Vercel
+export default app;
