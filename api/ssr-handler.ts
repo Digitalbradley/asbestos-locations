08bd14ce25fdb6df738545c10f583501fd59210d
Digitalbradley <digitalbradley@gmail.com>
2025-07-19 08:42:19 -0400

    Update ssr-handler.ts

--- api/ssr-handler.ts
+++ api/ssr-handler.ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import { generateFooterHTML } from './utils/footer-generator';
+import { generateFooterHTML } from './utils/footer-generator.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   console.log('🚀 SSR Handler started');
